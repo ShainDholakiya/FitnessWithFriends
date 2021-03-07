a488e3742b1121c46913cb5dfb13e77a1dead1a0
Shain Dholakiya <shainshain99@gmail.com>
2021-03-06 19:50:18 -0500

    login/register screens flow

--- app/src/main/java/com/example/fitnesswithfriends/MainActivity.java
+++ app/src/main/java/com/example/fitnesswithfriends/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.fitnesswithfriends;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Get Firebase auth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() == null) {
+            //User is not logged in
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+
         setContentView(R.layout.activity_main);
     }
+
 }
