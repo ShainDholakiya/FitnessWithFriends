13fa8ceb476f2cce90b683a28c2151a7b345a344
Shain Dholakiya <shainshain99@gmail.com>
2021-04-21 23:57:07 -0400

    code clea up

--- app/src/main/java/com/example/fitnesswithfriends/HomeActivity.java
+++ app/src/main/java/com/example/fitnesswithfriends/HomeActivity.java
@@ -2,36 +2,19 @@ package com.example.fitnesswithfriends;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,7 +30,6 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authListener;
     private FirebaseAuth mAuth;
     private FirebaseFirestore fStore;
-    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,33 +38,6 @@ public class HomeActivity extends AppCompatActivity {
         //Get Firebase auth instance
         mAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
-
-//        fStore.collection("workouts").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//
-//            }
-//        });
-
-
-//        fStore.collection("workouts").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//            @Override
-//            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//                if (queryDocumentSnapshots.isEmpty()) {
-//                    return;
-//                } else {
-//                    List<Object> allDocuments = new ArrayList<>(queryDocumentSnapshots.getDocuments());
-//                    for (int i = 0; i < allDocuments.size(); i++) {
-//                        Object document = allDocuments.get(i);
-//
-//                        System.out.println(document);
-//                    }
-////                    System.out.println(allDocuments);
-//                }
-//            }
-//        });
-
-        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         if (mAuth.getCurrentUser() == null) {
             //User is not logged in
@@ -130,34 +85,6 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void getMyWorkouts() {
-//        FirebaseDatabase.getInstance().getReference().child("workouts").addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                workouts.clear();
-//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                    Workout workout = snapshot.getValue(Workout.class);
-//
-//                    workouts.add(workout);
-//                    System.out.println("Hi");
-//
-////                    for (String id : idList) {
-////                        if (workout.getId().equals(userID)) {
-////                            workouts.add(workout);
-////                        }
-////                    }
-//                }
-//                System.out.println("------------------");
-//                System.out.println(workouts);
-////                Log.d("list f", workouts.toString());
-////                userAdapter.notifyDataSetChanged();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-
 
         fStore.collection("workouts").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
             @Override
