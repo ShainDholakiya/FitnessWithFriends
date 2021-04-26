60813560b4f06d84b3b4731725195548689293fe
cindyherrera <herreracindy9@gmail.com>
2021-04-26 02:58:57 -0400

    RelativeLayout changes

--- app/src/main/java/com/example/fitnesswithfriends/CreateProfileActivity.java
+++ app/src/main/java/com/example/fitnesswithfriends/CreateProfileActivity.java
@@ -1,8 +1,5 @@
 package com.example.fitnesswithfriends;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -23,6 +20,9 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -77,7 +77,7 @@ public class CreateProfileActivity extends AppCompatActivity implements View.OnC
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_profile);
+        setContentView(R.layout.activity_edit_profile);
 
         //Get Firebase auth instance
         fAuth = FirebaseAuth.getInstance();
